ae353b5f8cfe5c8873e2846f9a1b8a3464347ba7
is41-yarkovyua <y-yarkov@mail.ru>
2022-05-03 11:25:53 +0500

    version 1.0.9

--- resources/views/fourth.blade.php
+++ resources/views/fourth.blade.php
@@ -9,6 +9,41 @@
     <title>Document</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Main menu</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/second">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/third">Authorization</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="/fourth">Personal Area</a>
+                </li>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
 <div class="container d-flex justify-content-center align-items-center">
 
     <div class="card">
